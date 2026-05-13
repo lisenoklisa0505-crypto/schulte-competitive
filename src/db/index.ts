54228b465527a234aded5d4474799c48550fc97f
Bankmatt <oatamanchukk@mail.ru>
2026-05-14 02:35:37 +0600

    fix: switch to neon-http driver

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,6 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // отключаем проверку сертификата
-  },
-});
-
-export const db = drizzle(pool, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
